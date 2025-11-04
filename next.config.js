466b2cc4002968415371cdb2bb290726d4ec4c85
z-knot <zknot147@gmail.com>
2025-11-04 09:25:41 +0300

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -9,25 +9,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: { unoptimized: true },
-  experimental: {
-    serverComponentsExternalPackages: ['@prisma/client', 'bcryptjs'],
-  },
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      net: false,
-      tls: false,
-    };
-    return config;
-  },
-  swcMinify: true,
-};
-
-module.exports = nextConfig;
