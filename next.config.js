551127e85c431f1291c24e8236ed843cb24c3e45
z-knot <zknot147@gmail.com>
2025-11-04 09:31:20 +0300

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,22 @@
 /** @type {import('next').NextConfig} */
-// next.config.js
 const nextConfig = {
-  swcMinify: false,
-  experimental: {
-    swcLoader: true,
-    forceSwcTransforms: false,
+  eslint: {
+    ignoreDuringBuilds: true,
   },
+  images: { unoptimized: true },
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'bcryptjs'],
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    };
+    return config;
+  },
+  swcMinify: true,
 };
 
 module.exports = nextConfig;
